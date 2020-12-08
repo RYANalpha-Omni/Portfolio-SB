8f83c4d04b4fb77d1e7f30322721f6cc3f28e541
Ryan O'Neil <60462290+RYANalpha-Omni@users.noreply.github.com>
2020-12-08 21:01:49 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.menu-toggler').on('click', function() {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+    
+    $('#up').click(function() {
+        $('html,body').animate({scrollTop:0});
+        return false;
+    });
+
+    AOS.init({
+        easing:'ease',
+        duration:1800
+    });
+});
+
